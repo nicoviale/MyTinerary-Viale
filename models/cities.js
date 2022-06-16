5a9a1af3431fb5960b9454a74c96f84bdb3ef0ce
nicoviale <106348848+nicoviale@users.noreply.github.com>
2022-06-16 07:31:57 -0300

    update

---
+++ models/cities.js
@@ -0,0 +1,11 @@
+const mongoose= require('mongoose')
+
+const citiesSchema = new mongoose.Schema({
+    name:{type:String,required: true},
+    country:{type:String, required: true},
+    description:{type:String, required:true},
+    image:{type:String, required:true},
+})
+
+const Cities = mongoose.model('cities', citiesSchema)
+module.exports =Cities
